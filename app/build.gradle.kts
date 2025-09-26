66c3129b8b486a49b026fbc3b5b1dd75359764df
hshaon <147902647+hshaon@users.noreply.github.com>
2025-09-26 02:11:49 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,5 +57,14 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.3.0")
 
+    implementation("androidx.compose.material3:material3:1.2.1")
+
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
